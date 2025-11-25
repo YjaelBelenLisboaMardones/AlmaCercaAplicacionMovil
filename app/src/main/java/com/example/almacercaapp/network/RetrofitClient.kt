d7540bf2e0ced0cf19c5a5d3e29344ccd5d99dfe
yjadev <lisboayjael@gmail.com>
2025-11-25 17:29:02 -0300

    direccion de servidor agregada

--- app/src/main/java/com/example/almacercaapp/network/RetrofitClient.kt
+++ app/src/main/java/com/example/almacercaapp/network/RetrofitClient.kt
@@ -1,4 +1,18 @@
 package com.example.almacercaapp.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
+
+    // 👇 AQUÍ ESTÁ TU URL DE RAILWAY YA CONFIGURADA
+    private const val BASE_URL = "https://almacercabackend-production.up.railway.app/"
+
+    val instance: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
 }
